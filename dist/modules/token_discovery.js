bd27edd85722811dd2cb1c7c94332eea881b3c81
dmdeklerk <dennis@heatledger.com>
2020-10-13 10:06:15 +0200

    Include build in dist

--- dist/modules/token_discovery.js
+++ dist/modules/token_discovery.js
@@ -3,6 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.tokenDiscovery = void 0;
 const heat_server_common_1 = require("heat-server-common");
 const lodash_1 = require("lodash");
+class TokenDiscoveryResultEx {
+    constructor(parentResult) {
+        this.assetId = parentResult.assetId;
+        this.assetType = parentResult.assetType;
+        this.value = parentResult.value;
+        this.exists = parentResult.exists;
+    }
+}
+function nativeHeatDetails() {
+    return {
+        contract: '0',
+        decimals: 8,
+        name: 'Heat',
+        symbol: 'HEAT',
+    };
+}
+function assetHeatDetails(resp) {
+    const [symbol, name] = heat_server_common_1.tryParse(resp.properties) || ['*', '*'];
+    return {
+        contract: resp.id,
+        decimals: resp.decimals,
+        name,
+        symbol,
+    };
+}
 async function tokenDiscovery(context, param) {
     try {
         const { req, protocol, host, logger } = context;
@@ -19,6 +44,7 @@ async function tokenDiscovery(context, param) {
                         assetType: heat_server_common_1.AssetTypes.NATIVE,
                         value: d.unconfirmedBalance || '0',
                         exists: true,
+                        details: nativeHeatDetails()
                     });
                 }
                 else {
@@ -27,6 +53,7 @@ async function tokenDiscovery(context, param) {
                         assetType: heat_server_common_1.AssetTypes.TOKEN_TYPE_1,
                         value: d.unconfirmedBalance || '0',
                         exists: true,
+                        details: assetHeatDetails(d)
                     });
                 }
             });
@@ -37,6 +64,7 @@ async function tokenDiscovery(context, param) {
                 assetType: heat_server_common_1.AssetTypes.NATIVE,
                 value: '0',
                 exists: false,
+                details: nativeHeatDetails()
             });
         }
         return {
